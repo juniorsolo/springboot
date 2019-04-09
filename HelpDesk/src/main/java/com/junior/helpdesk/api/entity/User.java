43fa699a1f5c1b24be50e839488961cc0529df0e
juniorsolo <junior.solo@hotmail.com>
2019-04-08 21:19:47 -0300

    Criando entidades e Enums do sistema.
    Signed-off-by: juniorsolo <junior.solo@hotmail.com>

--- HelpDesk/src/main/java/com/junior/helpdesk/api/entity/User.java
+++ HelpDesk/src/main/java/com/junior/helpdesk/api/entity/User.java
@@ -8,6 +8,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.junior.helpdesk.enums.ProfileEnum;
+
 @Document
 public class User {
 	
@@ -22,6 +24,43 @@ public class User {
 	@NotBlank(message="Password is required.")
 	@Size(min = 6)
 	private String password;
+	
+	private ProfileEnum profile;
+
+	
+	//Getters and Setters
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ProfileEnum getProfile() {
+		return profile;
+	}
+
+	public void setProfile(ProfileEnum profile) {
+		this.profile = profile;
+	}
 	
 	
 }
---
+++ HelpDesk/src/main/java/com/junior/helpdesk/enums/PriorityEnum.java
@@ -0,0 +1,7 @@
+package com.junior.helpdesk.enums;
+
+public enum PriorityEnum {
+	High,
+	Normal,
+	Low
+}
