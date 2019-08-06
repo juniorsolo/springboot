42f28f44e0fba5756126231b7e520728cc14b40f
Manoel <manoel.junior.ext@sascar.com>
2019-08-06 18:32:53 -0300

    Adicionando update de usuário na controller.

--- HelpDesk/src/main/java/com/junior/helpdesk/api/controller/UserController.java
+++ HelpDesk/src/main/java/com/junior/helpdesk/api/controller/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,14 +54,57 @@ public class UserController {
 			response.getErros().add("Email already registration!");
 			return ResponseEntity.badRequest().body(response);
 		}catch (Exception e) {
-			
+			return ResponseEntity.badRequest().body(response);
 		}
 		return ResponseEntity.ok(response);
 	}
 	
 	private void validateCreateUser(User user, BindingResult result) {
-		if(user == null || user.getEmail() == null || user.getEmail().trim().isEmpty()) {
+		if(user != null && user.getEmail() == null || user.getEmail().trim().isEmpty()) {
 			result.addError(new ObjectError("User", "Email não informado"));
 		}
+	}
+	
+	@PutMapping
+	@PreAuthorize("hasAnyHole('ADMIN')")
+	public ResponseEntity<Response<User>> update(HttpServletRequest request, @RequestBody User user,
+			BindingResult result){
+		Response<User> response = new Response<>();
+		
+		try {
+			
+			validateUpdateUser(user, result);
+			
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErros().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			User persistedUser= userService.createOrUpdate(user);
+			response.setData(persistedUser);
+		}catch (Exception e) {
+			ResponseEntity.badRequest().body(response);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	private void validateUpdateUser(User user, BindingResult result) {
+		
+		if(user != null && user.getId() == null) {
+			result.addError(new ObjectError("User", "Id não informado!"));
+		}
+		
+		if(user != null && user.getEmail() != null) {
+			result.addError(new ObjectError("User", "Email não informado!"));
+		}
+		
+	}
+	
+	@GetMapping(value = "{id}")
+	@PreAuthorize("hasAnyHole('ADMIN')")
+	public ResponseEntity<User> findById(){
+		return null;
 	}
 }
