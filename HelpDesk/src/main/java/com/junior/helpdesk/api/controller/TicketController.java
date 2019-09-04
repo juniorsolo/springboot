22750a4c8243c16affb6a9cd7b991616f2f41879
junior <junior.solo@hotmail.com>
2019-09-04 00:25:16 -0300

    correction

--- HelpDesk/src/main/java/com/junior/helpdesk/api/controller/TicketController.java
+++ HelpDesk/src/main/java/com/junior/helpdesk/api/controller/TicketController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.mongodb.core.aggregation.AccumulatorOperators.Sum;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
