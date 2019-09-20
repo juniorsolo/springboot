9a592a7bc181a8c090276bbe612261f8bebe475e
Manoel <manoel.junior.ext@sascar.com>
2019-09-20 16:50:02 -0300

    add more menu

--- angularSpring/apps/angular/helpdesk/src/app/app.module.ts
+++ angularSpring/apps/angular/helpdesk/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
