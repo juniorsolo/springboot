46e3e6c52d10fd8157d57991224cb6f3ac4d8942
Manoel <manoel.junior.ext@sascar.com>
2019-09-24 18:01:15 -0300

    Adicionando componente para o footer e estrutura para o conteúdo principal.

--- angularSpring/apps/angular/helpdesk/src/app/app.module.ts
+++ angularSpring/apps/angular/helpdesk/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
