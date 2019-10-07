82dc8a7b8efb8dca85be01b46bab77b2857ca082
Manoel <manoel.junior.ext@sascar.com>
2019-10-07 19:11:46 -0300

    including shared service.

---
+++ angularSpring/apps/angular/helpdesk/src/app/services/shared.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { User } from '../model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  public static instance : SharedService = null;
+  user : User;
+  token : string;
+  showTemplate = new EventEmitter<boolean>();
+  constructor() { 
+    return SharedService.instance = SharedService.instance || this;
+  }
+
+  public static getInstance(){
+    if(this.instance == null){
+      this.instance = new SharedService();
+    }
+    return this.instance;
+  }
+
+  isLoggedIn():boolean{
+    return this.user == null ? false : this.user.email != '';
+  }
+}
