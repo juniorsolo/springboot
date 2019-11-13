14624ce4963f088b3fd192de7b5891345379fe8e
Manoel <manoel.junior.ext@sascar.com>
2019-11-13 20:14:03 -0300

    implementando...

--- angularSpring/apps/angular/helpdesk/src/app/components/ticket-new/ticket-new.component.ts
+++ angularSpring/apps/angular/helpdesk/src/app/components/ticket-new/ticket-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { SharedService } from 'src/app/services/shared.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ticket-new',
@@ -7,8 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketNewComponent implements OnInit {
 
-  constructor() { }
 
+  ticketForm : FormGroup;
+  shared : SharedService;
+  submitted = false;
+  message: {};
+  classCss : {};
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+    ) { 
+      this.shared = SharedService.getInstance();
+      this.createTicketForm();
+    }
+
+    createTicketForm() {
+      this.ticketForm = this.formBuilder.group({
+        id: [''],
+        number: [0],
+        title: [''],
+        status: [''],
+        priority: [''],
+        imagem: [''],
+        user: new FormGroup({
+          id: new FormControl(''),
+          email: new FormControl(''),
+          password: new FormControl(''),
+          profile: new FormControl('')
+        }),
+        assignedUser: new FormGroup({
+          id: new FormControl(''),
+          email: new FormControl(''),
+          password: new FormControl(''),
+          profile: new FormControl('')
+        }),
+        data: [''],
+        changes: [{}]
+      });
+    }
   ngOnInit() {
   }
 
