997fa0859c3f4d18ff43c17baea764fa15fc1a30
Manoel <manoel.junior.ext@sascar.com>
2019-11-27 18:07:31 -0300

    Adicionando tela para o detalhe do ticket.

--- angularSpring/apps/angular/helpdesk/src/app/components/ticket-detail/ticket-detail.component.ts
+++ angularSpring/apps/angular/helpdesk/src/app/components/ticket-detail/ticket-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Ticket } from 'src/app/model/ticket.model';
+import { SharedService } from 'src/app/services/shared.service';
+import { TicketService } from 'src/app/services/ticket.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ticket-detail',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketDetailComponent implements OnInit {
 
-  constructor() { }
+  ticket = new Ticket('',0,'','','','',null,null,'',null);
+  shared : SharedService;
+  message : {};
+  classCss : {};
+
+  constructor(
+    private ticketService : TicketService,
+    private route: ActivatedRoute
+  ) { 
+    this.shared = SharedService.getInstance();
+  }
 
   ngOnInit() {
   }
 
+  
 }
